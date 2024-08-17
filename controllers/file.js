826a4b1d9d4bb94e09a59e4fcda57686abcf9fba
Stalloyde <stalloyde93@gmail.com>
2024-08-17 12:17:33 +0800

    Format file date that is sent to client

--- controllers/file.js
+++ controllers/file.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const { format } = require('date-fns');
 
 exports.allFilesGET = async (req, res, next) => {
   try {
@@ -13,8 +14,25 @@ exports.allFilesGET = async (req, res, next) => {
   }
 };
 
-exports.fileGET = (req, res, next) => {
-  res.json('Load file page');
+exports.targetFileGET = async (req, res, next) => {
+  const targetId = Number(req.params.fileId);
+  try {
+    const targetFile = await prisma.file.findUnique({
+      where: { id: targetId },
+      include: {
+        user: {
+          select: { username: true },
+        },
+      },
+    });
+    targetFile.createdAt = format(
+      new Date(targetFile.createdAt),
+      'dd-MMM-yyyy',
+    );
+    return res.json(targetFile);
+  } catch (err) {
+    return res.json(err);
+  }
 };
 
 exports.createFile = (req, res, next) => {
