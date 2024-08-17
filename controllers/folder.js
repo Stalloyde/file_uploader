6517a09cb8116aaf2e232f620e38222a38bdff8e
Stalloyde <stalloyde93@gmail.com>
2024-08-17 14:50:17 +0800

    Complete feature - createFolder

--- controllers/folder.js
+++ controllers/folder.js
@@ -1,3 +1,5 @@
+const expressAsyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
@@ -36,9 +38,28 @@ exports.folderGET = async (req, res) => {
   }
 };
 
-exports.createFolder = (req, res) => {
-  res.json('Create folder');
-};
+exports.createFolder = [
+  body('newFolderName')
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage('*Input cannot be empty or only whitespace'),
+
+  expressAsyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ errors: errors.array() });
+    }
+
+    const newFolder = await prisma.folder.create({
+      data: {
+        folderName: req.body.newFolderName,
+        userId: req.user.id,
+      },
+    });
+    return res.json(newFolder);
+  }),
+];
 
 exports.fileInFolderGET = (req, res) => {
   res.json('Load file in folder page');
