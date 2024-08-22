7f37ae6e53f51176bc647a5b5c79c6a66d9f77c8
Stalloyde <stalloyde93@gmail.com>
2024-08-22 22:31:39 +0800

    set cookie sameSite to none and secure

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
+      sameSite: 'none',
+      secure: true,
     },
     store: new PrismaSessionStore(new PrismaClient(), {
       checkPeriod: 2 * 60 * 1000, //ms
